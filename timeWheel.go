ec443d0bfd2e12268d0909ed2b09732e1ae893be
stefan <2572915286@qq.com>
2020-12-02 12:26:14 +0800

    add time wheel pro.

---
+++ timeWheel.go
@@ -0,0 +1,238 @@
+package akTimeWheel
+
+import (
+	"ak-remote/common"
+	"container/list"
+	"sync"
+	"time"
+
+	"github.com/Peakchen/xgameCommon/akLog"
+)
+
+type TW int
+
+const (
+	TW_NO      TW = 0
+	TW_SmallMs TW = 1 //5ms  	(5-100]ms
+	TW_BigMs   TW = 2 //50ms 	(100-1000]ms
+	TW_Sec     TW = 3 //1s   	(1-60]s
+	TW_Min     TW = 4 //60s  	(1-60]min
+	TW_Day     TW = 5 //30min 	(1-24]h
+	TW_Month   TW = 6 //12h    (1-30]d
+	TW_Year    TW = 7 //15d    (1-12]month
+
+	TW_Max TW = TW_Year
+)
+
+type TWRange struct {
+	min int64
+	max int64
+}
+
+var (
+	twTickers = map[TW]time.Duration{
+		TW_SmallMs: 5 * time.Millisecond,
+		TW_BigMs:   50 * time.Millisecond,
+		TW_Sec:     1 * time.Second,
+		TW_Min:     60 * time.Second,
+		TW_Day:     30 * time.Minute,
+		TW_Month:   12 * time.Hour,
+		TW_Year:    15 * 24 * time.Hour,
+	}
+
+	twScale = map[TW]int64{
+		TW_SmallMs: 5,
+		TW_BigMs:   50,
+		TW_Sec:     common.Sec2Ms_Int64(1),
+		TW_Min:     common.Sec2Ms_Int64(60),
+		TW_Day:     common.Min2Ms_Int64(30),
+		TW_Month:   common.Hour2Ms_Int64(12),
+		TW_Year:    common.Day2Ms_Int64(15),
+	}
+
+	twRange = map[TW]*TWRange{
+		TW_SmallMs: &TWRange{min: 5, max: 100},
+		TW_BigMs:   &TWRange{min: 100, max: 1000},
+		TW_Sec:     &TWRange{min: common.Sec2Ms_Int64(1), max: common.Sec2Ms_Int64(60)},
+		TW_Min:     &TWRange{min: common.Min2Ms_Int64(1), max: common.Min2Ms_Int64(60)},
+		TW_Day:     &TWRange{min: common.Hour2Ms_Int64(1), max: common.Hour2Ms_Int64(24)},
+		TW_Month:   &TWRange{min: common.Day2Ms_Int64(1), max: common.Day2Ms_Int64(30)},
+		TW_Year:    &TWRange{min: common.Month2Ms_Int64(1), max: common.Month2Ms_Int64(12)},
+	}
+)
+
+const (
+	TaskElementMax = 1000
+)
+
+type akTimerMgr struct {
+	tws  map[TW]*TimeWheel
+	ext  chan *TaskElement
+	stop chan bool
+}
+
+type TaskElement struct {
+	ty   TW
+	task *list.Element
+}
+
+var (
+	_akTimerMgr *akTimerMgr
+)
+
+func Start() {
+	_akTimerMgr = &akTimerMgr{
+		tws:  make(map[TW]*TimeWheel, TW_Max),
+		ext:  make(chan *TaskElement, TaskElementMax),
+		stop: make(chan bool, 1),
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go _akTimerMgr.wheelLoop(&wg)
+
+	for wi := TW_SmallMs; wi <= TW_Max; wi++ {
+		wg.Add(1)
+		_akTimerMgr.tws[wi] = &TimeWheel{
+			ty:    wi,
+			tasks: list.New(),
+		}
+		go _akTimerMgr.tws[wi].taskLoop(_akTimerMgr.ext, &wg)
+	}
+}
+
+func AddTimer(ty TW, interval int64, runs int64, fn func()) {
+	_akTimerMgr.addTask(ty, interval, runs, fn)
+}
+
+/*
+	ty :  time wheel type
+	interval : task time interval
+	runs : time wheel run times
+	fn : function call back
+*/
+func (this *akTimerMgr) addTask(ty TW, interval int64, runs int64, fn func()) {
+	tRange, ok := twRange[ty]
+	if !ok {
+		akLog.Error("can not find tw range data, ty: ", ty)
+		return
+	}
+	if tRange.min > interval || tRange.max < interval {
+		akLog.Error("tw interval over range, ty: ", ty, interval)
+		return
+	}
+	this.tws[ty].add(interval, runs, fn)
+}
+
+func (this *akTimerMgr) wheelLoop(wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for {
+		select {
+		case ele := <-this.ext:
+			this.tws[ele.ty].pushBack(ele.task.Value.(*WheelTask))
+		}
+	}
+}
+
+func getTimewheelScale(src TW, interval int64) (dst TW) {
+	for i := src - 1; i >= TW_SmallMs; i-- {
+		if (interval > 0 && twRange[i].min >= interval) ||
+			(twRange[i].min < interval && twRange[i].max >= interval) {
+			return i
+		}
+	}
+	return TW_NO
+}
+
+type TimeWheel struct {
+	ty    TW
+	tasks *list.List
+}
+
+type WheelTask struct {
+	create   int64
+	start    int64
+	interval int64
+	runs     int64
+	fn       func()
+	scales   int64
+	mark     int64
+}
+
+func (this *WheelTask) reset() {
+	this.mark = 0
+}
+
+func (this *TimeWheel) add(interval int64, runs int64, fn func()) {
+	this.tasks.PushBack(&WheelTask{
+		create:   time.Now().Unix(),
+		interval: interval,
+		runs:     runs,
+		fn:       fn,
+	})
+}
+
+func (this *TimeWheel) pushBack(e interface{}) {
+	this.tasks.PushBack(e)
+}
+
+func (this *TimeWheel) pop(e interface{}) {
+	this.tasks.Remove(e.(*list.Element))
+}
+
+func (this *TimeWheel) taskLoop(ext chan *TaskElement, wg *sync.WaitGroup) {
+	ticker := time.NewTicker(twTickers[this.ty])
+	defer func() {
+		ticker.Stop()
+		wg.Done()
+	}()
+
+	for {
+		select {
+		case <-ticker.C:
+			if this.tasks.Len() == 0 {
+				continue
+			}
+
+			akLog.FmtPrintf("cur ty: %v.", this.ty)
+			e := this.tasks.Front()
+			if e != nil {
+
+				task := e.Value.(*WheelTask)
+				if task.mark == 0 && task.start == 0 {
+					task.start = time.Now().Unix()
+				}
+
+				task.scales += int64(twScale[this.ty])
+				subscale := task.interval - task.scales
+				if subscale <= 0 {
+					task.scales = 0
+					task.mark++
+					task.fn()
+				} else if subscale < twScale[this.ty] && subscale > 0 {
+					dst := getTimewheelScale(this.ty, subscale)
+					if dst != TW_NO {
+						akLog.FmtPrintf("get next ty: %v, src: %v, subscale: %v.", dst, this.ty, subscale)
+						ext <- &TaskElement{
+							ty: dst,
+							task: &list.Element{
+								Value: &WheelTask{
+									create:   time.Now().Unix(),
+									interval: subscale,
+									runs:     1,
+									fn:       task.fn,
+								},
+							},
+						}
+						task.scales = 0
+						task.mark++
+					}
+				}
+				if task.mark == task.runs {
+					this.pop(e)
+				}
+			}
+		}
+	}
+}
---
+++ wheel_test.go
@@ -0,0 +1,112 @@
+package akTimeWheel
+
+import (
+	"ak-remote/common"
+	"testing"
+	"time"
+
+	"github.com/Peakchen/xgameCommon/akLog"
+)
+
+func init() {
+	Start()
+}
+
+//TW_SmallMs TW = 1 //5ms  	(5-100]ms
+func Test_TW_SmallMs(t *testing.T) {
+	var exit = make(chan int, 1)
+	var count int64
+	var interval int64 = 5
+	var runs int64 = 1000
+	var now = time.Now()
+	AddTimer(TW_SmallMs, interval, runs, func() {
+		count++
+		akLog.FmtPrintln("TW_SmallMs hello...", count)
+		if count >= runs && runs > 0 {
+			exit <- 1
+		}
+	})
+	select {
+	case <-exit:
+		akLog.FmtPrintln("TW_SmallMs exit...", time.Since(now))
+	}
+}
+
+//TW_BigMs   TW = 2 //50ms 	(100-1000]ms
+func Test_TW_BigMs(t *testing.T) {
+	var exit = make(chan int, 1)
+	var count int64
+	var interval int64 = 101
+	var runs int64 = 2
+	var now = time.Now()
+	AddTimer(TW_BigMs, interval, runs, func() {
+		count++
+		akLog.FmtPrintln("TW_BigMs hello...", count)
+		if count >= runs && runs > 0 {
+			exit <- 1
+		}
+	})
+	select {
+	case <-exit:
+		akLog.FmtPrintln("TW_BigMs exit...", time.Since(now))
+	}
+}
+
+//TW_Sec     TW = 3 //1s   	(1-60]s
+func Test_TW_Sec(t *testing.T) {
+	var exit = make(chan int, 1)
+	var count int64
+	var interval int64 = common.Sec2Ms_Float64(2.101)
+	var runs int64 = 2
+	var now = time.Now()
+	AddTimer(TW_Sec, interval, runs, func() {
+		count++
+		akLog.FmtPrintln("TW_Sec hello...", count)
+		if count >= runs && runs > 0 {
+			exit <- 1
+		}
+	})
+	select {
+	case <-exit:
+		akLog.FmtPrintln("TW_Sec exit...", time.Since(now))
+	}
+}
+
+//TW_Min     TW = 4 //60s  	(1-60]min
+func Test_TW_Min(t *testing.T) {
+	var exit = make(chan int, 1)
+	var count int64
+	var interval int64 = common.Min2Ms_Float64(1.101)
+	var runs int64 = 2
+	var now = time.Now()
+	AddTimer(TW_Min, interval, runs, func() {
+		count++
+		akLog.FmtPrintln("TW_Min hello...", count)
+		if count >= runs && runs > 0 {
+			exit <- 1
+		}
+	})
+	select {
+	case <-exit:
+		akLog.FmtPrintln("TW_Min exit...", time.Since(now))
+	}
+}
+
+func TestTimeWheel(t *testing.T) {
+	var exit = make(chan int, 1)
+	var count int64
+	var interval int64 = 2
+	var runs int64 = 2
+
+	AddTimer(TW_Sec, interval, runs, func() {
+		count++
+		akLog.FmtPrintln("hello...", count)
+		if count >= runs {
+			exit <- 1
+		}
+	})
+	select {
+	case <-exit:
+		akLog.FmtPrintln("exit...")
+	}
+}
